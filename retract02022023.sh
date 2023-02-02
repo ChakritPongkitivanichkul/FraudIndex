e31f2d443e1bef59d5c32d2f510ff7d419c53cc1
ChakritPongkitivanichkul <118002126+ChakritPongkitivanichkul@users.noreply.github.com>
2023-02-02 22:01:45 +0700

    Add files via upload

---
+++ retract02022023.sh
@@ -0,0 +1,7 @@
+python3 main.py 57222058913
+python3 main.py 57280388500
+python3 main.py 57205441866
+python3 main.py 57218762417
+python3 main.py 57281021700
+python3 main.py 57225904708
+python3 main.py 57221767465
